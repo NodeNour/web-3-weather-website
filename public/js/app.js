62cc3f7f49d5dc8fe61975861d8aaffaedb623c5
unknown <nodevjs@gmail.com>
2019-07-19 21:11:16 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client side javascript...')
+
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+messageOne.textContent = 'Loading...'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = searchElement.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    
+    response.json().then((data) => {
+        if (data.errorMsg) {
+            console.log(data.errorMsg)
+            messageOne.textContent = data.errorMsg
+        } else {
+            console.log(data.forecast)
+            console.log(data.location)
+            messageOne.textContent = data.forecast.temperature + ' ... ' + data.forecast.rainProb
+            messageTwo.textContent = data.location
+        }
+    })
+}) 
+    //console.log(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude,longitude,callback) => {
+
+    const url = 'https://api.darksky.net/forecast/f4d2a1a95c8e8fd3064d15673e3899a6/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable', undefined)
+        } else if (body.error) {
+            callback('Another unable', undefined)
+        } else {
+            callback(undefined, {
+                temperature: Math.round((body.currently.temperature - 32) *(5/9)),
+                rainProb: body.currently.precipProbability*100
+            })
+        }
+    })
+}
+
+module.exports = forecast
