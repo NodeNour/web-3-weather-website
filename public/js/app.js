bb2a7c43ddd4bdc1d9fdbaf9d4c4f309a7538895
unknown <nodevjs@gmail.com>
2019-07-20 01:30:48 +0100

    Setup changes to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (event) => {
 
     const location = searchElement.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     
     response.json().then((data) => {
         if (data.errorMsg) {
